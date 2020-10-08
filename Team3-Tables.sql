26137bc3069a49d4c6de14dd739e2bb48205ffc7
CJ Danyluk <Nickdan5743@gmail.com>
2020-10-08 14:07:14 -0400

    added prologue

---
+++ Team3-Tables.sql
@@ -0,0 +1,7 @@
+#  Hardware Online
+#  
+#  Authors: Fioti, Figueroa, Danyluk
+#
+#  We are located at 12345 Poughkeepsie Lane, Poughkeepsie New York 12550
+#  Phone Number: 845-666-6969
+#  Email: admin@hardwareonline.com
