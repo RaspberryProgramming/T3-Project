0d18e73ea1e2e520710ffb5a5c31e158df1c3b5f
RaspberryPi99 <camimportantemails@gmail.com>
2020-10-27 17:10:03 -0400

    Fixed some styling issues

--- public/admin/dispusers.php
+++ public/admin/dispusers.php
@@ -38,32 +38,37 @@
               }
             include "../nav.php";
 
+  ?>
+  <main>
+  <?php
+    require "../../connect_db.php";
 
-require "../../connect_db.php";
+    $table = "T3_users";
 
-$table = "T3_users";
+    $q = "SELECT * FROM $table";
 
-$q = "SELECT * FROM $table";
-
-$r = mysqli_query ($dbc, $q);
+    $r = mysqli_query ($dbc, $q);
 
 
-echo "<table border=1> <th>Name</th> <th>Password</th> <th>Hashtype</th> <th>Rank ID</th> <th>Active</th>";
+    echo "<table border=1> <th>Name</th> <th>Password</th> <th>Hashtype</th> <th>Rank ID</th> <th>Active</th>";
 
-if ($r) 
-{ echo "<br>Query Successful!";
-echo "<br> Showing $table: " ;
-while ($row = mysqli_fetch_array( $r, MYSQLI_NUM)) {
-echo "<br> <tr> <td>$row[0]</td> <td>$row[1]</td>". 
-                    "<td>$row[2]</td> <td>$row[3]</td><td>$row[4]</td></tr>";
-} 
+    if ($r) 
+      {
+      echo "<br>Users";
+      while ($row = mysqli_fetch_array( $r, MYSQLI_NUM)) {
+         echo "<br> <tr> <td>$row[0]</td> <td>$row[1]</td>". 
+                        "<td>$row[2]</td> <td>$row[3]</td><td>$row[4]</td></tr>";
+     } 
 
-echo "</table>";
-}
-else {
-    echo "<br>Query Unsuccessful!";
-}		
-
+      echo "</table>";
+    }
+    else {
+        echo "<br>Database Error!!!";
+    }		
+    ?>
+    </main>
+    
+    <?php
     include "../footer.php";
     ?>
 
