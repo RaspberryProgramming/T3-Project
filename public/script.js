450ab9428d1cbf48dc0cd43e1aae35d98301b72e
RaspberryProgramming <camimportantemails@gmail.com>
2020-10-29 17:10:36 -0400

    Added max-age to disclaimer cookie

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 function eulaAgree() {
   document.getElementById("disclaimer-overlay").style.cssText =
     "visibility:hidden";
-  document.cookie = "disclaimer=True";
+  document.cookie = "disclaimer=True; max-age=2592000";
   localStorage.setItem("disclaimer", "true");
 }
