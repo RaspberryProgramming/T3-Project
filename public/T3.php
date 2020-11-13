3f9a69e761a42f5fe3641508e6bdda48d7c8b301
RaspberryProgramming <camimportantemails@gmail.com>
2020-11-13 16:30:52 -0500

    Added Carousel to homepage

--- public/T3.php
+++ public/T3.php
@@ -40,9 +40,41 @@
             include "nav.php";
       ?>
     <main>
-      <img alt="img.logo-large" class="logo-large" src="logo.webp" />
-      <h1>Hardware Online</h1>
-      <p>America's Favorite Online Hardware Store</p>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="carousel-caption" style="display: table-cell; vertical-align: middle;">
+            <img class="logo-large" src="logo.webp" alt="First slide">
+            <div class="text">
+              <h1>Hardware Online</h1>
+              <p>America's Favorite Online Hardware Store</p>
+            </div>
+          </div>
+          <img class="d-block w-100" src="slide-1.webp" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="slide-2.webp" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="slide-3.webp" alt="Third slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+      <!--<img alt="img.logo-large" class="logo-large" src="logo.webp" />-->
+      
 
     </main>
 
