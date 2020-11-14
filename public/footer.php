dafbbc4b64a6aa5f3195551d11f048f6db0b92a3
RaspberryProgramming <camimportantemails@gmail.com>
2020-11-14 11:24:51 -0500

    Changed filenames to fit convention

--- public/footer.php
+++ public/footer.php
@@ -6,15 +6,15 @@
           <a href="mailto:admin@hardwareonline.com">admin@hardwareonline.com</a>
         </p>
           |
-        <a href="/Changelog.php">Changelog</a>
+        <a href="/changelog.php">Changelog</a>
           |
-        <a href="/Disclaimer.php">Disclaimer</a>
+        <a href="/disclaimer.php">Disclaimer</a>
 
         </div>
       <p>(C) 2020
       <?php echo FILE_AUTHOR; ?>
       All Rights Reserved</p>
       <?php
-echo "<p>Version: <a href='/Changelog.php'>" . FILE_VERSION . "</a></p>";
+echo "<p>Version: <a href='/changelog.php'>" . FILE_VERSION . "</a></p>";
 ?>
 </footer>
---
+++ public/phpinfo.php
@@ -0,0 +1,7 @@
+<!-- If the php does runs, it will make this line dissapear-->
+<p style="color: red;<?php echo "display:none;"; ?>">PHP is Not Working</p>
+
+<?php
+# If php runs it will print out that the PHP is working.
+echo "<p style='color:green;'>PHP is Working</p>";
+?>
