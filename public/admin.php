b105c29f22e33325c00a7efcd00fba619fa53e48
RaspberryProgramming <camimportantemails@gmail.com>
2020-11-14 11:14:57 -0500

    Added Logos to Website

--- public/admin.php
+++ public/admin.php
@@ -46,20 +46,43 @@ include "nav.php";
         <div class="flex-col-item row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
           <div class="col p-4 d-flex flex-column position-static">
             <strong class="d-inline-block mb-2 text-primary">PHP INFO</strong>
+            <p>
+              <svg width="5em" height="5em" viewBox="0 0 16 16" class="bi bi-info-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/>
+                <circle cx="8" cy="4.5" r="1"/>
+              </svg>
+            </p>
             <p class="card-text mb-auto"><?php include "./admin/phpinfo.php"?></p>
           </div>
         </div>
+        <a href="tables.php">
         <div class="flex-col-item row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
           <div class="col p-4 d-flex flex-column position-static">
-            <strong class="d-inline-block mb-2 text-primary">Table Editor</strong>
-            <p class="card-text mb-auto"><a href="tables.php"> Click to enter Table Editor </a></p>
+              <strong class="d-inline-block mb-2 text-primary">Table Editor</strong>
+              <p>
+                <svg width="5em" height="5em" viewBox="0 0 16 16" class="bi bi-table" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 2h-4v3h4V4zm0 4h-4v3h4V8zm0 4h-4v3h3a1 1 0 0 0 1-1v-2zm-5 3v-3H6v3h4zm-5 0v-3H1v2a1 1 0 0 0 1 1h3zm-4-4h4V8H1v3zm0-4h4V4H1v3zm5-3v3h4V4H6zm4 4H6v3h4V8z"/>
+                </svg>
+              </p>
+              <p class="card-text mb-auto"> Click to enter Table Editor </p>
           </div>
         </div>
+        </a>
+
         </div>
       <a class="flex-col-item" href="/admin/projectdocument.php">
         <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
           <div class="col p-4 d-flex flex-column position-static">
             <strong class="d-inline-block mb-2 text-primary">PROJECT DOCUMENTATION</strong>
+            <p>
+              <svg width="5em" height="5em" viewBox="0 0 16 16" class="bi bi-file-earmark-text" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
+                <path d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z"/>
+                <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+            </p>
             <p class="card-text mb-auto">Click to view project documentation</p>
           </div>
         </div>
--- public/shop.php
+++ public/shop.php
@@ -106,12 +106,16 @@ $id = $_GET["id"]; # unique id of the entry that we want to delete
 
             echo "<h1>Shop</h1>";
 
-            echo "<table class='shop-table'>";
+            echo "<br><table class='shop-table'>";
 
             # Takes each row and prints each of it's elements in order
             while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
                 echo "<tr>";
-                echo "<td><a href='?id=$row[0]'>$row[3]</a></td><td>Stock: $row[4]</td><td>Price: $row[6]$</td>";
+                
+                echo "<td><p><a href='?id=$row[0]'>$row[3]</a></p><p><b>Stock:</b> $row[4]</p><p><b>Price:</b> $row[6]$</p></td>";
+                echo "<td><a href='?id=$row[0]'><svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-cart' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>";
+                echo "<path fill-rule='evenodd' d='M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z'/>";
+                echo "</svg></a></td>";
                 echo "</tr>";
             }
 
@@ -136,7 +140,7 @@ $id = $_GET["id"]; # unique id of the entry that we want to delete
 
                 # Beggining of product page
                 echo "<div style='width:80%;'>";
-                echo "<h1>$row[3]</h1>"; # Product Name
+                echo "<h1>$row[3]</h1><br>"; # Product Name
 
                 #Query for the product's supplier
                 $q = "select * from T3_suppliers WHERE vendorid=$row[1] AND active=1";
@@ -150,13 +154,19 @@ $id = $_GET["id"]; # unique id of the entry that we want to delete
                 }
 
                 if ($row[4] >= 1) { # If the product is in stock
-                echo "<p><b style='color: green;'>In Stock</b></p>"; # Show that it is in stock
-                echo "<p>Stock: $row[4]</p>"; # Stock Quantity
-                echo "<a class='btn btn-success' href='purchase.php?id=$row[0]'>Buy Now: $$row[6]</a>"; # Show Buy Now Button
-            } else {
-                    echo "<p><b style='color: red;'>Out Of Stock</b></p>"; # Display out of stock
+                    echo "<p style='color: red;'>$$row[6]</p>";
+                    echo "<p><b style='color: green;'>In Stock</b></p>"; # Show that it is in stock
+                    echo "<p>Stock: $row[4]</p>"; # Stock Quantity
+                    echo "<a class='btn btn-success' href='purchase.php?id=$row[0]'>";
+                    echo "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-cart-plus' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>";
+                    echo "<path fill-rule='evenodd' d='M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z'/>";
+                    echo "<path fill-rule='evenodd' d='M8.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 .5-.5z'/>";
+                    echo "</svg>";
+                    echo " Buy Now</a>"; # Show Buy Now Button
+                } else {
+                        echo "<p><b style='color: red;'>Out Of Stock</b></p>"; # Display out of stock
                 }
-                echo "<br><p><b>Description:</b>$row[5]</p>"; # Display the description
+                echo "<br><br><p><b>Description:</b><br>$row[5]</p>"; # Display the description
 
             }
             if ($exists == false) {
