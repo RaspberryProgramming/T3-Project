d4943cee62120e631076e4f99a8e0c79f869d8cb
RaspberryProgramming <camimportantemails@gmail.com>
2020-11-17 16:17:00 -0500

    Added css to limit size of navbar logo

--- public/nav.php
+++ public/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="/T3.php"><img src="/logo.webp"></a>
+      <a class="navbar-brand" href="/T3.php"><img class="mini-logo" src="/logo.webp"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -8,11 +8,13 @@
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
             <a class="nav-link" href="/T3.php">
-              Home<span class="sr-only">(current)</span></a>
+              Home
+              <span class="sr-only">(current)</span>
+            </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/aboutus.php">About Us</a>
-          </li> 
+          </li>
           <li class="nav-item">
             <a class="nav-link" href="/shop.php">Shop</a>
           </li>
