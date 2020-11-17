43e02a27201b90a6ebb7016cecf8f0007749114e
RaspberryProgramming <camimportantemails@gmail.com>
2020-11-17 16:24:29 -0500

    Put anchor around whole item

--- public/shop.php
+++ public/shop.php
@@ -21,7 +21,7 @@
   <title>Hardware Online</title>
 </head>
 
-<body> 
+<body>
   <?php
 # Global Variable Definitions
 define("FILE_AUTHOR", "Fioti, Figueroa, Danyluk");
@@ -111,8 +111,8 @@ $id = $_GET["id"]; # unique id of the entry that we want to delete
             while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
                 echo "<tr>";
 
-                echo "<td><p><a href='?id=$row[0]'>$row[3]</a></p><p><b>Stock:</b> $row[4]</p><p><b>Price:</b> $row[6]$</p></td>";
-                echo "<td><a href='?id=$row[0]'><svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-cart' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>";
+                echo "<td><a href='?id=$row[0]'><p>$row[3]</p><p><b>Stock:</b> $row[4]</p><p><b>Price:</b> $row[6]$</p></td>";
+                echo "<td><svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-cart' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>";
                 echo "<path fill-rule='evenodd' d='M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z'/>";
                 echo "</svg></a></td>";
                 echo "</tr>";
