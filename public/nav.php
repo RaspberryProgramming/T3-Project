9670ec081f94c54899afcd6b0b529245eeb34f84
RaspberryProgramming <camimportantemails@gmail.com>
2020-11-17 16:23:59 -0500

    Rearranged Navbar

--- public/nav.php
+++ public/nav.php
@@ -1,33 +1,34 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="/T3.php"><img class="mini-logo" src="/logo.webp"></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+  <a class="navbar-brand" href="/T3.php"><img class="mini-logo" src="/logo.webp"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="/T3.php">
-              Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/aboutus.php">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/shop.php">Shop</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/login.php">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/admin.php">Admin</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/tables.php">Table Editor</a>
-          </li>
-        </ul>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/T3.php">
+          Home
+          <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/shop.php">Shop</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/tables.php">Table Editor</a>
+      </li>
 
-      </div>
-    </nav>
+      <li class="nav-item">
+        <a class="nav-link" href="/login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/admin.php">Admin</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/aboutus.php">About Us</a>
+      </li>
+    </ul>
+
+  </div>
+</nav>
