f74ef65a75171e49e7c5fb465e912de5f4035313
RaspberryProgramming <camimportantemails@gmail.com>
2020-11-21 12:52:48 -0500

    Added some comments to script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 function eulaAgree() {
-  document.getElementById("disclaimer-overlay").style.cssText =
+  /* Used to allow users to agree to disclaimer and store a cookie to prevent the disclaimer from coming back*/
+  document.getElementById("disclaimer-overlay").style.cssText = // Hide the disclaimer
     "visibility:hidden";
-  document.cookie += "disclaimer=True; max-age=2592000";
-  localStorage.setItem("disclaimer", "true");
+  document.cookie += "disclaimer=True; max-age=2592000"; // Set the cookie to prevent the disclaimer from coming up again
 }
