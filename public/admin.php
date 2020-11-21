f6b2f6d57b9005a65563084af4d0401ed5ed137d
RaspberryProgramming <camimportantemails@gmail.com>
2020-11-21 12:51:24 -0500

    Moved phpinfo.php into admin.php instead of include

--- public/admin.php
+++ public/admin.php
@@ -54,7 +54,15 @@ include "nav.php";
                 <circle cx="8" cy="4.5" r="1"/>
               </svg>
             </p>
-            <p class="card-text mb-auto"><?php include "phpinfo.php"?></p>
+            <p class="card-text mb-auto">
+              <!-- If the php does runs, it will make this line dissapear-->
+              <p style="color: red;<?php echo "display:none;"; ?>">PHP is Not Working</p>
+
+              <?php
+              # If php runs it will print out that the PHP is working.
+              echo "<p style='color:green;'>PHP is Working</p>";
+              ?>
+            </p>
           </div>
         </div>
         <a href="tables.php">
